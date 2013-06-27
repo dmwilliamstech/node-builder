a0bc18d6a89db1a73b02b0637ab3faed5c44368b
kellyp <kellyplummer@mac.com>
2013-06-27 16:46:13 -0700

    use original url for deployment

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -75,6 +75,6 @@ grails.project.dependency.resolution = {
 
 grails.project.dependency.distribution = {
     localRepository = "~/.m2"
-    remoteRepository(id:"codiceSnapshots", url:"https://artifacts.codice.org/content/repositories/snapshots/")
+    remoteRepository(id:"codiceSnapshots", url:"https://tools.codice.org/artifacts/content/repositories/snapshots/")
     remoteRepository(id:"codiceReleases", url:"https://artifacts.codice.org/content/repositories/releases/")
 }
