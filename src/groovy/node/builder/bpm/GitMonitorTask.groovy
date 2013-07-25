294f1f7d704ea2f737502eaa1c73ebf7a60fc2d5
kellyp <kellyplummer@mac.com>
2013-07-25 13:57:50 -0700

    initial git monitor task

---
+++ src/groovy/node/builder/bpm/GitMonitorTask.groovy
@@ -0,0 +1,65 @@
+package node.builder.bpm
+
+import node.builder.Manifest
+import node.builder.Utilities
+import org.activiti.engine.delegate.DelegateExecution
+import org.activiti.engine.delegate.JavaDelegate
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.lib.StoredConfig
+
+
+class GitMonitorTask implements JavaDelegate{
+
+    void execute(DelegateExecution delegateExecution) throws Exception {
+        //check dir exists
+        def remotePath = delegateExecution.getVariable("remotePath")
+        def localPath = delegateExecution.getVariable("localPath")
+
+        def localCopy = new File(localPath)
+        if(!localCopy.exists()){
+            Git.cloneRepository()
+                    .setURI(remotePath)
+                    .setDirectory(localCopy)
+                    .call();
+            assert new File(localPath).exists()
+            delegateExecution.setVariable("repositoryDidChange", false)
+            return
+        }
+        def localRepo = new FileRepository(localPath + "/.git");
+        def git = new Git(localRepo)
+
+        def pullResult = git.pull()
+                            .call()
+
+        def results = [:]
+        results.successful = pullResult.successful
+
+        def fetchResult = pullResult.fetchResult
+        results.advertisedRefs = fetchResult.advertisedRefs
+        results.messages = fetchResult.messages
+        results.trackingRefUpdates = fetchResult.trackingRefUpdates
+        results.uri = fetchResult.getURI()
+
+        def mergeResult = pullResult.mergeResult
+        if(mergeResult){
+            results.mergeBase = mergeResult.base
+            results.mergeCheckoutConflicts = mergeResult.checkoutConflicts
+            results.mergeConflicts = mergeResult.conflicts
+            results.mergeFailingPaths = mergeResult.failingPaths
+            results.mergedCommits = mergeResult.mergedCommits
+            results.mergeStatus = mergeResult.mergeStatus
+            results.mergeNewHead = mergeResult.newHead
+        }
+
+        def rebaseResult = pullResult.rebaseResult
+        if(rebaseResult){
+            results.rebaseConflicts = rebaseResult.conflicts
+            results.rebaseCurrentCommit = rebaseResult.currentCommit
+            results.rebaseFailingPaths = rebaseResult.failingPaths
+            results.rebaseStatus = rebaseResult.status
+        }
+        delegateExecution.setVariable("repositoryDidChange", !fetchResult.trackingRefUpdates.empty)
+        delegateExecution.setVariable("repositoryChangeResults", results)
+    }
+}
---
+++ test/unit/node/builder/GitMonitorTaskTests.groovy
@@ -0,0 +1,119 @@
+package node.builder
+
+import grails.converters.JSON
+import grails.test.mixin.TestMixin
+import grails.test.mixin.web.ControllerUnitTestMixin
+import groovy.mock.interceptor.MockFor
+import node.builder.bpm.GitMonitorTask
+import org.activiti.engine.delegate.DelegateExecution
+import org.eclipse.jgit.api.CreateBranchCommand
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.internal.storage.file.FileRepository
+import org.eclipse.jgit.lib.StoredConfig
+import org.eclipse.jgit.revwalk.RevCommit
+import org.eclipse.jgit.revwalk.RevCommitList
+import org.eclipse.jgit.transport.RemoteRefUpdate
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Before
+
+@TestMixin(ControllerUnitTestMixin)
+class GitMonitorTaskTests {
+    def localPath = File.createTempFile("local", Long.toString(System.nanoTime())).getPath()
+    def remotePath = File.createTempFile("remote", Long.toString(System.nanoTime())).getPath()
+    Git git
+    Git remoteGit
+
+    @Before
+    void setup(){
+        def workingCopy = new File(localPath)
+        assert workingCopy.delete()
+        localPath = workingCopy.absolutePath
+
+        def remoteCopy = new File(remotePath)
+        assert remoteCopy.delete()
+        remotePath = remoteCopy.absolutePath
+        //setup repo
+        setupRemoteRepo(remoteCopy)
+    }
+
+    def setupLocalRepo(workingCopy){
+        Git.cloneRepository()
+                .setURI(remotePath)
+                .setDirectory(workingCopy)
+                .call();
+
+        def localRepo = new FileRepository(localPath + "/.git");
+        git = new Git(localRepo)
+
+        assert workingCopy.exists()
+        assert (new File(localPath + "/.git")).exists()
+    }
+
+    def setupRemoteRepo(remoteCopy){
+        assert Git.init()
+                .setDirectory(remoteCopy)
+                .call()
+
+        new File("${remoteCopy.absolutePath}/thisisatest.txt").write("test ${Long.toString(System.nanoTime())}\n")
+        def remoteRepo = new FileRepository(remoteCopy.absolutePath + "/.git");
+        remoteGit = new Git(remoteRepo)
+        remoteGit.add()
+            .addFilepattern("thisisatest.txt")
+            .call()
+
+        remoteGit.commit()
+            .setMessage("initial test repo")
+            .call()
+    }
+
+    def changeRepo(){
+        new File("${remotePath}/thisisatest.txt").append("test ${Long.toString(System.nanoTime())}\n")
+
+        def result = remoteGit.commit()
+                .setMessage("testing change")
+                .setOnly("thisisatest.txt")
+                .call()
+        assert result.id != null
+    }
+
+    def mockDelegateExecutionWithVariables(variables, variablecount){
+        def delegateExecution = new MockFor(DelegateExecution.class)
+
+        delegateExecution.demand.getVariable(){variable -> variables[variable]}
+        delegateExecution.demand.getVariable(){variable -> variables[variable]}
+
+        (0..variablecount).each {
+            delegateExecution.demand.setVariable(){name, value -> variables[name] = value}
+        }
+        return delegateExecution.proxyInstance()
+    }
+
+    def testCloneInitial(){
+        def task = new GitMonitorTask()
+        def variables = [localPath: localPath, remotePath: remotePath]
+        task.execute(mockDelegateExecutionWithVariables(variables,1))
+
+        assert (new File(localPath)).exists()
+        assert !variables.repositoryDidChange
+    }
+
+    def testDetectChange(){
+        def task = new GitMonitorTask()
+        setupLocalRepo(new File(localPath))
+        changeRepo()
+        def variables = [localPath: localPath, remotePath: remotePath]
+        task.execute(mockDelegateExecutionWithVariables(variables,2))
+
+        assert variables.repositoryDidChange
+    }
+
+    def testDetectNoChange(){
+        def task = new GitMonitorTask()
+        setupLocalRepo(new File(localPath))
+        def variables = [localPath: localPath, remotePath: remotePath]
+        task.execute(mockDelegateExecutionWithVariables(variables,2))
+        assert !variables.repositoryDidChange
+    }
+
+}
