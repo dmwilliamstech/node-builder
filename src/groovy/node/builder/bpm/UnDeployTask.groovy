ea5b0a555296151e627168083f87ad67135257cb
kellyp <kellyplummer@mac.com>
2013-08-29 17:09:14 -0700

    use the absolute path to the pems

--- src/groovy/node/builder/bpm/UnDeployTask.groovy
+++ src/groovy/node/builder/bpm/UnDeployTask.groovy
@@ -13,7 +13,14 @@ class UnDeployTask implements JavaDelegate{
         for(instance in deployment.instances){
             try{
 //                Process p = "puppet cert clean #{instance.name}.novalocal".execute()
-                "curl --cacert ~/.ssh/ca_crt.pem --cert ~/.ssh/stackbox_cert.novalocal.pem --key ~/.ssh/stackbox_pk.novalocal.pem -k -X DELETE -H \"Accept: pson\" https://localhost:8140/production/certificate_status/${instance.name}.novalocal".execute()
+                Process process = "curl --cacert /usr/share/tomcat6/.ssh/ca_crt.pem --cert /usr/share/tomcat6/.ssh/stackbox_cert.novalocal.pem --key /usr/share/tomcat6/.ssh/stackbox_pk.novalocal.pem -k -X DELETE -H \"Accept: pson\" https://localhost:8140/production/certificate_status/${instance.name}.novalocal".execute()
+                process.waitFor()
+                if(process.exitValue() > 0){
+                    log.error "Uh-oh there seems to be an issue ${process.err.text}"
+                }else{
+                    log.info "Cleaned puppet cert ${process.text}"
+                }
+
             }catch(e){
                 log.error "failed to remove certification for ${instance.name}.novalocal - ${e.getMessage()}"
                 delegateExecution.setVariable("result", [error: [message: "Failed to unregister instance ${instance.name} ${e.getMessage()}"]])
