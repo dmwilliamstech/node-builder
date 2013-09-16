42db35e9ccbe921d90f91a4fd248e6136000dce9
kellyp <kellyplummer@mac.com>
2013-09-16 10:32:27 -0700

    use local repo for testing

--- test/functional/node/builder/ProjectListTest.groovy
+++ test/functional/node/builder/ProjectListTest.groovy
@@ -16,14 +16,12 @@
 
 package node.builder
 
-import geb.junit4.GebReportingTest
-import groovy.json.JsonSlurper
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.springframework.core.io.ClassPathResource
 
-class ProjectListTest extends NodeBuilderFunctionalTest{
+class ProjectListTest extends NodeBuilderFunctionalTestBase{
 
     @Before
     void setup(){
@@ -32,7 +30,7 @@ class ProjectListTest extends NodeBuilderFunctionalTest{
         project.description = "Test"
         project.bpmn = new ClassPathResource("resources/process.xml").getFile().text
         project.active = false
-        project.location = "git@github.com:kellyp/testy.git"
+        project.location = createEmptyRepo().path
         project.processDefinitionKey = "gitChangeMonitor"
         project.projectType = (ProjectType.findByName("GIT Repository"))
 
@@ -46,7 +44,7 @@ class ProjectListTest extends NodeBuilderFunctionalTest{
         login()
         go('project')
         assert title == "Project List"
-        sleep(10000)
+        sleep(100)
         assert $('[id^=project]').first().text() == ("Test")
         assert $('.icon-pencil') != null
     }
@@ -55,5 +53,7 @@ class ProjectListTest extends NodeBuilderFunctionalTest{
     void teardown(){
         Project.where { id > 0l }.deleteAll()
         assert Project.count == 0
+
+        deleteEmptyRepo()
     }
 }
