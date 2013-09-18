3b7940603fc8c90e4e3c1635ea9165fb2dfd94cc
kellyp <kellyplummer@mac.com>
2013-09-18 14:30:59 -0700

    updated logging settings

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -95,22 +95,23 @@ log4j = {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
-//    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
-//           'org.codehaus.groovy.grails.web.pages',          // GSP
-//           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
-//           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-//           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
-//           'org.codehaus.groovy.grails.commons',            // core / classloading
-//           'org.codehaus.groovy.grails.plugins',            // plugins
-//           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
-//           'org.springframework',
-//           'org.hibernate',
-//           'net.sf.ehcache.hibernate',
-//           'node.builder'
+    error  'org.codehaus.groovy.grails.web.servlet',        // controllers
+           'org.codehaus.groovy.grails.web.pages',          // GSP
+           'org.codehaus.groovy.grails.web.sitemesh',       // layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping',        // URL mapping
+           'org.codehaus.groovy.grails.commons',            // core / classloading
+           'org.codehaus.groovy.grails.plugins',            // plugins
+           'org.codehaus.groovy.grails.orm.hibernate',      // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
 
-    root {
-        info 'stdout', 'file'
-    }
+    info   'node.builder'
+
+//    root {
+//        info 'stdout', 'file'
+//    }
 }
 
 grails.config.defaults.locations = [KickstartResources]
