6618b96c77e8b72c8d0afba312b9fab917190993
kellyp <kellyplummer@mac.com>
2013-10-08 13:11:21 -0700

    attach the users org to the project and use it for access

--- grails-app/controllers/node/builder/ProjectController.groovy
+++ grails-app/controllers/node/builder/ProjectController.groovy
@@ -18,6 +18,7 @@ package node.builder
 
 import grails.converters.JSON
 import org.apache.catalina.connector.Response
+import org.springframework.dao.DataIntegrityViolationException
 
 /**
  * ProjectController
@@ -27,11 +28,103 @@ class ProjectController {
     def springSecurityService
     def projectService
 
-    static expose = 'project'
-    static scaffold = true
-//	def index = { }
-    static allowedMethods = [run: "POST"]
 
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST", run: "POST"]
+
+    def index() {
+        redirect(action: "list", params: params)
+    }
+
+    def list() {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [projectInstanceList: projectService.findAllByOrganizations(new TreeSet<String>(springSecurityService.principal.organizations), params), projectInstanceTotal: Project.count()]
+    }
+
+    def create() {
+        [projectInstance: new Project(params)]
+    }
+
+    def save() {
+        def projectInstance = projectService.createWithoutSaving(params, springSecurityService.principal.organizations)
+        if (!projectInstance.save(flush: true)) {
+            render(view: "create", model: [projectInstance: projectInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.created.message', args: [message(code: 'project.label', default: 'Project'), projectInstance.id])
+        redirect(action: "show", id: projectInstance.id)
+    }
+
+    def show() {
+        def projectInstance = projectService.getByOrganizations(params.id, new TreeSet<String>(springSecurityService.principal.organizations))
+        if (!projectInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        [projectInstance: projectInstance]
+    }
+
+    def edit() {
+        def projectInstance = projectService.getByOrganizations(params.id, new TreeSet<String>(springSecurityService.principal.organizations))
+        if (!projectInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        [projectInstance: projectInstance]
+    }
+
+    def update() {
+        def projectInstance = Project.get(params.id)
+        if (!projectInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        if (params.version) {
+            def version = params.version.toLong()
+            if (projectInstance.version > version) {
+                projectInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                          [message(code: 'project.label', default: 'Project')] as Object[],
+                          "Another user has updated this Project while you were editing")
+                render(view: "edit", model: [projectInstance: projectInstance])
+                return
+            }
+        }
+
+        projectInstance.properties = params
+
+        if (!projectInstance.save(flush: true)) {
+            render(view: "edit", model: [projectInstance: projectInstance])
+            return
+        }
+
+		flash.message = message(code: 'default.updated.message', args: [message(code: 'project.label', default: 'Project'), projectInstance.id])
+        redirect(action: "show", id: projectInstance.id)
+    }
+
+    def delete() {
+        def projectInstance = Project.get(params.id)
+        if (!projectInstance) {
+			flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "list")
+            return
+        }
+
+        try {
+            projectInstance.delete(flush: true)
+			flash.message = message(code: 'default.deleted.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "list")
+        }
+        catch (DataIntegrityViolationException e) {
+			flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'project.label', default: 'Project'), params.id])
+            redirect(action: "show", id: params.id)
+        }
+    }
 
     def run(){
         def project = Project.get(params.id)
