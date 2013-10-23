08821e32b9c0e271502709d8a9ca22e4d42b095c
kellyp <kellyplummer@mac.com>
2013-10-23 15:06:24 -0700

    pretty print when converting to json

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -56,7 +56,9 @@ grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
 // The default codec used to encode data with ${}
 grails.views.default.codec = "none" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
-grails.converters.encoding = "UTF-8"
+
+grails.converters.json.pretty.print = true
+grails.converters.default.pretty.print = true
 // enable Sitemesh preprocessing of GSP pages
 grails.views.gsp.sitemesh.preprocess = true
 // scaffolding templates configuration
